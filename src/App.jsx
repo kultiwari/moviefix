a50e86a9f8d283d11eb1822170af4c16c01340a8
kultiwari <148486929+kultiwari@users.noreply.github.com>
2024-06-07 00:32:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,8 @@ import { getApiConfiguration, getGenres } from "./store/homeSlice";
 
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
-import Home from "./pages/home/Home";
 import Details from "./pages/details/Details";
-import SearchResult from "./pages/searchResult/SearchResult";
+import Home from "./pages/home/Home";
 import Explore from "./pages/explore/Explore";
 import PageNotFound from "./pages/404/PageNotFound";
 
@@ -57,8 +56,10 @@ function App() {
 
     return (
         <BrowserRouter>
-        
+     
             <Routes>
+                
+            <Route path="/" element={<Home />} />
             <Route path="/:mediaType/:id" element={<Details />} />      
                 <Route path="/explore/:mediaType" element={<Explore />} />
                 <Route path="*" element={<PageNotFound />} />
